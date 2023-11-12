a904ed11f7c02de9f363f5e94da5367b12489f09
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2023-11-12 01:14:51 -0500

    Reworked components - added router - and made basic login

---
+++ src/app/pages/login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TextBox from "../components/textBox.jsx";
+import Button from "../components/button.jsx";
+
+export default class Login extends React.Component {
+	render() {
+		return (
+			<div
+				style={{
+					height: "100%",
+					display: "grid",
+					placeItems: "center",
+				}}>
+				<form
+					style={{
+						display: "inline-flex",
+						justifyContent: "center",
+						alignItems: "center",
+						flexDirection: "column",
+						height: "100%",
+						minWidth: "20em",
+						gap: "1em",
+						fontSize: "1.25rem",
+					}}>
+					<TextBox placeholder="Email" name="email" />
+					<TextBox placeholder="Password" type="password" name="password" />
+					<Button type="submit">Login</Button>
+				</form>
+			</div>
+		);
+	}
+}
